87aa93864b6ca86a81590a130d3fad26bf578a5d
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-07-30 11:01:24 -0300

    frontend e backend estão funcionandos e comunicando entre si

--- backend/verificador_cpf.php
+++ backend/verificador_cpf.php
@@ -1,2 +1,53 @@
 <?php
-echo "Nada no backend por enquanto!";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/plain; charset=UTF-8");
+
+$cpf = $_POST["cpf"] ?? null;
+$aux;
+$result;
+
+//PRIMEIRA ETAPA
+if (preg_match('/^(\d)\1{10}$/', $cpf)) {
+    $result = false;
+}
+
+
+//SEGUNDA ETAPA
+for ($i = 0; $i < 9; $i++) {
+    $digito = (int)$cpf[$i];
+    $peso = 10 - $i;
+    $aux += $digito * $peso;
+}
+
+$aux = ($aux * 10) % 11;
+
+if($aux == $cpf[9]){
+    $result = true;
+}else{
+    $result = false;
+}
+
+
+
+//TERCEIRA ETAPA
+$aux = 0;
+for ($i = 0; $i < 10; $i++) {
+        $digito = (int)$cpf[$i];
+        $peso = 11 - $i;
+        $aux += $digito * $peso;
+    }
+
+$aux = ($aux * 10) % 11;
+
+if($aux == $cpf[10]){
+    $result = true;
+}else{
+    $result = false;
+}
+
+if($result){
+    echo "CPF válido!";
+}else{
+    echo "CPF inválido!";
+}
+?>
--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 function Form() {
 
   const [cpf, setCpf] = useState("");
+  
+  //coloque a url do php aqui!
+  const url = "http://localhost/projetosphp/verificador_cpf_backend/verificador_cpf.php"
 
   const handleChange = (e) => {
     let value = e.target.value.replace(/\D/g, "");
@@ -11,14 +14,31 @@ function Form() {
     if (value.length > 7) value = value.replace(/(\d{3})(\d)/, "$1.$2");
     if (value.length > 11) value = value.replace(/(\d{3})(\d{2})$/, "$1-$2");
     setCpf(value);
-  };
+  }
+
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+  const cpf_for_php = cpf.replace(/\D/g, "");
+
+  const result = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: `cpf=${encodeURIComponent(cpf_for_php)}`
+  });
+
+  const texto = await result.text();
+  alert(texto);
+};
+
+  
   
   return (
     <>
-      <form action="" style={{display:'flex', flexDirection: 'column'}}>
-        <label>Digite o CPF:</label>
-        <input type="text" value={cpf} onChange={handleChange} placeholder="xxx.xxx.xxx-xx" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" required/>
-        <button onClick={console.log("não há nada aqui por enquanto!")}>Verificar</button>
+      <form onSubmit={handleSubmit} style={{display:'flex', flexDirection: 'column'}}>
+        <label htmlFor="cpf">Digite o CPF:</label>
+        <input type="text" name='cpf' value={cpf} onChange={handleChange} placeholder="xxx.xxx.xxx-xx" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" required/>
+        <button type='submit'>Verificar</button>
       </form>
     </>
   )
